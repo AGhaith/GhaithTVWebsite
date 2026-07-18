c8747fd1d1922ef2cc77883f527a9165162db549
Ahmed Ghaith <a.wesam2300@nu.edu.eg>
2026-07-18 07:48:30 +0300

    chore: release links updated to v1.1.3

--- src/App.jsx
+++ src/App.jsx
@@ -211,12 +211,12 @@ function App() {
               
               <div className="download-options">
                 <div className="btn-wrapper">
-                  <a href="https://github.com/AGhaith/GhaithTVWebsite/releases/download/GhaithTV-Android-V1.1.2/GhaithTV.apk" className="btn btn-primary glass">
+                  <a href="https://github.com/AGhaith/GhaithTVWebsite/releases/download/GhaithTV-Android-V1.1.3/GhaithTV.apk" className="btn btn-primary glass">
                     <Download size={20} /> {t.btnAndroid}
                   </a>
                 </div>
                 <div className="btn-wrapper">
-                  <a href="https://github.com/AGhaith/GhaithTVWebsite/releases/download/GhaithTV-Windows-V1.1.2/Ghaith.TV.Setup.1.1.2.exe" className="btn btn-secondary glass">
+                  <a href="https://github.com/AGhaith/GhaithTVWebsite/releases/download/GhaithTV-Windows-V1.1.3/Ghaith.TV.Setup.1.1.3.exe" className="btn btn-secondary glass">
                     <MonitorPlay size={20} /> {t.btnWindows}
                   </a>
                 </div>
